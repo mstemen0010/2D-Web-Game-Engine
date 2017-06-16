c63f3364e5a27f707bc3500746bf43ce1d36f3e5
Matt Stemen <matthew.stemen@thermofisher.com>
2017-06-16 16:03:39 -0500

    Started engine code

---
+++ public_html/src/Engine/Engine_Core.js
@@ -0,0 +1,49 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+"use scrict";
+
+var gEngine = gEngine || {}; // singlton
+
+gEngine.Core = (function () {
+    // instance variable: the graphical context for drawing
+    var mGL = null;
+
+    // Accessor of the webgl context 
+    var getGL = function () {
+        return mGL;
+    };
+
+    // Contains the functions and variables that will be accessible.
+
+    var mPublic = {
+        getGL: getGL,
+        initializeWebGL: initializeWebGL,
+        clearCanvas: clearCanvas
+    };
+
+    return mPublic;
+}());
+
+// Clears the draw area and draws one square
+var clearCanvas = function(color) {
+    mGL.clearColor(color[0], color[1], color[2], color[3]); // set the color to be cleared
+    mGL.clear(mGL.COLOR_BUFFER_BIT); // clear to the color previously set
+};
+
+// initialize the WebGL, the vertex buffer and compile the shaders
+var initializeWebGL = function (htmlCanvasID) {
+    var canvas = document.getElementById(htmlCanvasID);
+// Get the standard or experimental webgl and binds to the Canvas area
+// store the results to the instance variable mGL
+    mGL = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+    if (mGL === null) {
+        document.write("<br><b>WebGL is not supported!</b>");
+        return;
+    }
+// now initialize the VertexBuffer
+    gEngine.VertexBuffer.initialize();
+};
