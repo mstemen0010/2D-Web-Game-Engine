ff947effa6e9a342363884935279a123165be90f
Matt Stemen <matthew.stemen@thermofisher.com>
2017-06-16 15:03:03 -0500

    added shader

--- public_html/src/ShaderSupport.js
+++ public_html/src/ShaderSupport.js
@@ -4,6 +4,8 @@
  * and open the template in the editor.
  */
 
+/* global gGL */
+
 var gSimpleShader = null;
 var gShaderVertexPositionAttribute = null;
 
@@ -23,4 +25,30 @@ function loadAndCompileShader(id, shaderType) {
                 gGL.getShaderInfoLog(compiledShader));
     }
     return compiledShader;
+}
+function initSimpleShader(vertexShaderID, fragmentShaderID) {
+// Step A: load and compile the vertex and fragment shaders
+    var vertexShader = loadAndCompileShader(vertexShaderID, gGL.VERTEX_SHADER);
+    var fragmentShader = loadAndCompileShader(fragmentShaderID,
+            gGL.FRAGMENT_SHADER);
+// Step B: Create and link the shaders into a program.
+    gSimpleShader = gGL.createProgram();
+    gGL.attachShader(gSimpleShader, vertexShader);
+    gGL.attachShader(gSimpleShader, fragmentShader);
+    gGL.linkProgram(gSimpleShader);
+// Step C: check for error
+    if (!gGL.getProgramParameter(gSimpleShader, gGL.LINK_STATUS))
+        alert("Error linking shader");
+// Step D: Gets a reference to the aSquareVertexPosition attribute
+    gShaderVertexPositionAttribute = gGL.getAttribLocation(gSimpleShader,
+            "aSquareVertexPosition");
+// Step E: Activates the vertex buffer loaded in VertexBuffer.js
+    gGL.bindBuffer(gGL.ARRAY_BUFFER, gSquareVertexBuffer);
+// Step F: Describe the characteristic of the vertex position attribute
+    gGL.vertexAttribPointer(gShaderVertexPositionAttribute,
+            3, // each vertex element is a 3-float (x,y,z)
+            gGL.FLOAT, // data type is FLOAT
+            false, // if the content is normalized vectors
+            0, // number of bytes to skip in between elements
+            0); // offsets to the first element
 }
